26a8e5f24b1624c73dd423482d367058d9a019f2
Benue <84378547+inu9431@users.noreply.github.com>
2026-02-09 00:26:16 +0900

    Revert "feat: Add multi-cloud user model and JWT authentication (#10)"
    This reverts commit 9582f10d6dbea2b8cb45b9ed4e1a6c80808e26c7.

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,3 @@
 from django.urls import path
 
-from apps.users.views import LoginView, LogoutView, SignupView
-
-urlpatterns = [
-    path("signup/", SignupView.as_view(), name="signup"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
-]
+urlpatterns = []
